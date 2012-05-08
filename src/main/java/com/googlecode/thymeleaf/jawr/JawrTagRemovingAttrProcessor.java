803c0bad8505c67cf513085c14593b15a24bd85e
FingolfinTEK@gmail.com <FingolfinTEK@gmail.com@eb045038-b128-15ba-dc94-d7078a0c0458>
2012-05-08 09:01:44 +0000

    Empty output bugfix

--- src/main/java/com/googlecode/thymeleaf/jawr/JawrTagRemovingAttrProcessor.java
+++ src/main/java/com/googlecode/thymeleaf/jawr/JawrTagRemovingAttrProcessor.java
@@ -2,6 +2,8 @@ package com.googlecode.thymeleaf.jawr;
 
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.NestableNode;
+import org.thymeleaf.dom.Node;
 import org.thymeleaf.processor.IAttributeNameProcessorMatcher;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.attr.AbstractAttrProcessor;
@@ -31,7 +33,13 @@ public class JawrTagRemovingAttrProcessor extends AbstractAttrProcessor {
 	@Override
 	public ProcessorResult processAttribute(final Arguments p_arguments, final Element p_element, final String p_attributeName) {
 		processor.processAttribute(p_arguments, p_element, p_attributeName);
-		p_element.getParent().removeChild(p_element);
+		final NestableNode parent = p_element.getParent();
+		
+		for(final Node child : p_element.getChildren()) {
+			parent.addChild(child);
+		}
+		
+		parent.removeChild(p_element);
 		return ProcessorResult.OK;
 	}
 
